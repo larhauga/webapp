b1e58dd74a7cacf9502c5c5fc213404226f8e546
Lars Haugan <lars@larshaugan.net>
2015-05-01 18:59:54 +0000

    First version of webapp
    Implements versioning in its own file
    config contains the different needed relations to enable external
    requests, so that the api will query for sub-services.

---
+++ webapp.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+import requests
+import argparse
+from flask import Flask
+app = Flask(__name__)
+
+config = ConfigParser.ConfigParser()
+config.read('etc/app.cfg')
+
+
+def get_version():
+    version = None
+    with open('etc/version.txt', 'r') as f:
+        version = f.read().strip().split()
+    return version
+
+
+@app.route("/")
+def front():
+    version = get_version()
+
+    return "Service %s. Version %s" % (version[0], version[1])
+
+
+@app.route('/api')
+def depend():
+    txt = "Service %s: %s<br />" % (str(get_version()[0]),
+                                    str(get_version()[1]))
+    txt += "<b>Sub services:</b><br />"
+    for service in config.get('main', 'service').split(','):
+        try:
+            r = requests.get(config.get(service, 'url'), timeout=1)
+            txt += r.text
+        except:
+            txt += "No connection to configured subservice"
+            print "Something went wrong"
+        txt += "<br />"
+    return txt
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, required=True)
+    args = parser.parse_args()
+    app.debug = config.get('main', 'debug')
+    app.run(host='0.0.0.0', port=args.port)
